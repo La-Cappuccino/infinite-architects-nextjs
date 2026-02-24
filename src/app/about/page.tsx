556656ad60a111ddec46bbd24596d7d985a6ef51
La-Cappuccino <echoalgoridata@gmail.com>
2026-02-24 23:40:52 +0100

    Add founder images to About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import PageLayout from "@/components/PageLayout";
 
 const values = [
@@ -20,6 +21,7 @@ const values = [
 const founders = [
   {
     name: "Russell",
+    image: "/images/team/russell.jpg",
     bio: `has always been inspired by the combination of the tangible and the intangible, a fascination that continues to this day. It was therefore almost inevitable that his chosen career would be Architecture. As through design, it unites the physical qualities of built form, material application, response to the environment and applicable technology with the creative arts, mathematical theories of form and proportion, human emotional response and history. This dichotomy pervades everything he does whether it is work related or social.
 
 With his 25 years' experience he has delivered a wide range of landmark and award winning projects ranging from industrial, residential, retail, commercial and high end hospitality for clients from Africa, Asia, Europe and the Middle East. Clients include the V&A Waterfront, Old Mutual, Sew Eurodrive, Robertson and Cain, Jumeirah Group, One & Only Resorts, Southern Sun, Fairmont, Hilton, Emaar, Mediclinic, City Hospital and Diyar.
@@ -30,6 +32,7 @@ He continues sailing here at Dubai Offshore Sailing Club in his spare time on hi
   },
   {
     name: "Helen",
+    image: "/images/team/helen.jpg",
     bio: `has always had a passion for design. With a Scottish RAF pilot Father and Swedish Mother, she grew up moving around the world, living in the UK, America & The Sultanate of Oman, finally settling in Dubai in 1998. She attained an Interior Design degree from Napier University in Edinburgh and has accumulated over 25 years' experience in the Hospitality Design industry. During this time, she has earned a reputation as a dynamic, multi-disciplined award-winning designer. Her vast knowledge and experience of hotel design comes from years of working closely with many of the world's leading hotel operators in conjunction with many prestigious clients and project teams. Whether it be master planning, concept design, detailed coordination with other consultants or site work, Helen achieves excellence by immersing herself in all facets of the design. Her career has seen her work on a multitude of internationally recognised projects spanning Europe, South America, Africa, and the Middle East. These range from luxury hotels, bars, fine dining restaurants and private villas. Her work and interaction with people from these varied regions have given her an insight into the diverse cultures of the world and a love of travel that inspires her creativity. Helen believes that being sensitive to the Client's needs, matched with an impeccable eye for detail wins every time.`,
   },
 ];
@@ -99,17 +102,49 @@ export default function AboutPage() {
           ))}
         </div>
 
+        {/* Founder Images - Side by Side */}
+        <div
+          style={{
+            maxWidth: "90%",
+            margin: "0 auto",
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr",
+            gap: "0",
+          }}
+        >
+          {founders.map((founder) => (
+            <div
+              key={founder.name}
+              style={{
+                position: "relative",
+                aspectRatio: "1/1",
+                overflow: "hidden",
+              }}
+            >
+              <Image
+                src={founder.image}
+                alt={founder.name}
+                fill
+                style={{
+                  objectFit: "cover",
+                  filter: "grayscale(100%)",
+                }}
+              />
+            </div>
+          ))}
+        </div>
+
         {/* Green Divider */}
         <div
           style={{
             maxWidth: "90%",
-            margin: "0 auto 60px",
+            margin: "40px auto",
             height: "2px",
             backgroundColor: "#80AE50",
           }}
         />
 
-        {/* Founders - 2 Columns */}
+        {/* Founders Bios - 2 Columns */}
         <div
           style={{
             maxWidth: "90%",
